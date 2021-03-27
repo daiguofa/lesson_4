838b7e3b90906be59dc8d837380333ec81c9f1ea
Jie Hou <hh_163com@163.com>
2021-03-27 12:50:39 +0800

    lesson 1+2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import 'core-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './lesson1/App';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {App}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -13,3 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
